9c82e5435c11a2504de3ab5583ec828859275714
Mücahit Kaldirim <muecahit-kaldirim@hotmail.de>
2026-06-08 18:43:44 +0200

    added sections

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,31 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './features/navbar/navbar';
+import { Hero } from './features/hero/hero';
+import { About} from './features/about/about';
+import { Skills } from './features/skills/skills';
+import { Projects } from './features/projects/projects';
+import { Testimonials } from './features/testimonials/testimonials';
+import { Contact } from './features/contact/contact';
+import { Footer } from './core/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    Navbar,
+    Hero,
+    About,
+    Skills,
+    Projects,
+    Testimonials,
+    Contact,
+    Footer
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('portfolio');
+export class AppComponent {
+  title = 'portfolio';
 }
