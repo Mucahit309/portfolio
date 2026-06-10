539d75e294a22c48d5858a817b00e6cbb78e9175
Mücahit Kaldirim <muecahit-kaldirim@hotmail.de>
2026-06-10 09:28:18 +0200

    feat: hero section style: hero added: icons

--- src/app/features/hero/hero.ts
+++ src/app/features/hero/hero.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
+  standalone: true,
   imports: [],
   templateUrl: './hero.html',
-  styleUrl: './hero.scss',
+  styleUrl: './hero.scss'
 })
-export class Hero {}
+export class Hero {
+  mouseX = 0;
+  mouseY = 0;
+
+  @HostListener('document:mousemove', ['$event'])
+  updateCursorPosition(event: MouseEvent) {
+    this.mouseX = event.clientX;
+    this.mouseY = event.clientY;
+  }
+}
