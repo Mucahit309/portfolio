ff640980e0fd2dc7eecf6b39997115a982384dba
Mücahit Kaldirim <muecahit-kaldirim@hotmail.de>
2026-06-19 11:36:02 +0200

    feat: projects dialog

--- src/app/features/projects/projects.ts
+++ src/app/features/projects/projects.ts
@@ -1,17 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild, inject } from '@angular/core';
 import { ProjectsService, Project } from '../../services/projects/projects';
 
 @Component({
   selector: 'app-projects',
+  standalone: true,
   templateUrl: './projects.html',
   styleUrls: ['./projects.scss']
 })
-export class Projects implements OnInit {
-  projectsList: Project[] = [];
+export class Projects {
+  projectsService = inject(ProjectsService);
+  projectsList = this.projectsService.getProjects();
 
-  constructor(private projectsService: ProjectsService) {}
+  @ViewChild('projectDialog') projectDialog!: ElementRef<HTMLDialogElement>;
 
-  ngOnInit() {
-    this.projectsList = this.projectsService.getProjects();
+  currentProjectIndex = 0;
+  selectedProject: Project | null = null;
+
+  openDialog(index: number) {
+    this.currentProjectIndex = index;
+    this.selectedProject = this.projectsList[index];
+    this.projectDialog.nativeElement.showModal();
+    document.body.style.overflow = 'hidden';
+  }
+
+  closeDialog() {
+    this.projectDialog.nativeElement.close();
+    document.body.style.overflow = 'auto';
+  }
+
+  nextProject() {
+    this.currentProjectIndex = (this.currentProjectIndex + 1) % this.projectsList.length;
+    this.selectedProject = this.projectsList[this.currentProjectIndex];
+  }
+
+  onDialogClick(event: MouseEvent) {
+    if (event.target === this.projectDialog.nativeElement) {
+      this.closeDialog();
+    }
   }
 }
--- src/app/services/projects/projects.ts
+++ src/app/services/projects/projects.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
 
 export interface Project {
+  id: string;
   title: string;
   technologies: string[];
   image: string;
+  description: string;
+  githubUrl: string;
+  liveUrl: string;
+  techIcons: { icon: string; name: string }[];
 }
 
 @Injectable({
@@ -12,14 +17,34 @@ export interface Project {
 export class ProjectsService {
   private projects: Project[] = [
     {
+      id: '01',
       title: 'Join',
       technologies: ['HTML', 'CSS', 'JavaScript'],
-      image: 'assets/projects/join.png'
+      image: 'assets/projects/join2.png',
+      description: 'A task management system based on the Kanban board. Create and organize tasks using drag and drop functions, assign users and categories.',
+      githubUrl: 'https://github.com/mucahit309',
+      liveUrl: '#',
+      techIcons: [
+        { icon: 'assets/projects/angular.svg', name: 'Angular' },
+        { icon: 'assets/projects/typescript.svg', name: 'TypeScript' },
+        { icon: 'assets/projects/html.svg', name: 'HTML' },
+        { icon: 'assets/projects/css.svg', name: 'CSS' },
+        { icon: 'assets/projects/firebase.svg', name: 'Firebase' }
+      ]
     },
     {
+      id: '02',
       title: 'El Pollo Loco',
       technologies: ['HTML', 'CSS', 'JavaScript'],
-      image: 'assets/projects/polloloco.png'
+      image: 'assets/projects/polloloco2.png',
+      description: 'A jump, run and throw game based on an object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.',
+      githubUrl: 'https://github.com/mucahit309',
+      liveUrl: '#',
+      techIcons: [
+        { icon: 'assets/projects/js.svg', name: 'JavaScript' },
+        { icon: 'assets/projects/html.svg', name: 'HTML' },
+        { icon: 'assets/projects/css.svg', name: 'CSS' }
+      ]
     }
   ];
 
