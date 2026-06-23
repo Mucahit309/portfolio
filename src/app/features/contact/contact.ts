11b6e547573083cfd548cf22201a19f704f8e23c
Mücahit Kaldirim <muecahit-kaldirim@hotmail.de>
2026-06-23 09:16:31 +0200

    feat: "contact me" section style: "contact me"

--- src/app/features/contact/contact.ts
+++ src/app/features/contact/contact.ts
@@ -1,9 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './contact.html',
-  styleUrl: './contact.scss',
+  styleUrls: ['./contact.scss']
 })
-export class Contact {}
+export class Contact {
+  private fb = inject(FormBuilder);
+
+  contactForm: FormGroup = this.fb.group({
+    name: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    message: ['', Validators.required],
+    privacyPolicy: [false, Validators.requiredTrue]
+  });
+
+  isSubmitting = false;
+  submitSuccess = false;
+  submitError = false;
+
+  async onSubmit() {
+    if (this.contactForm.valid) {
+      this.isSubmitting = true;
+      this.submitError = false;
+      this.submitSuccess = false;
+
+      const FORMSPREE_ENDPOINT = 'https://formspree.io/f/mrewgebb'; 
+
+      if (FORMSPREE_ENDPOINT === 'https://formspree.io/f/mrewgebb') {
+        setTimeout(() => {
+          this.isSubmitting = false;
+          this.submitSuccess = true;
+          this.contactForm.reset();
+          setTimeout(() => this.submitSuccess = false, 5000);
+        }, 1500);
+        return;
+      }
+
+      try {
+        const response = await fetch(FORMSPREE_ENDPOINT, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+          },
+          body: JSON.stringify(this.contactForm.value)
+        });
+
+        if (response.ok) {
+          this.isSubmitting = false;
+          this.submitSuccess = true;
+          this.contactForm.reset();
+          setTimeout(() => this.submitSuccess = false, 5000);
+        } else {
+          throw new Error('Network Error');
+        }
+      } catch (error) {
+        this.isSubmitting = false;
+        this.submitError = true;
+      }
+    } else {
+      this.contactForm.markAllAsTouched();
+    }
+  }
+}
