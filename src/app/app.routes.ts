c150e66ada6a5223a4cfc298ad39b8919db35ecb
Mücahit Kaldirim <muecahit-kaldirim@hotmail.de>
2026-06-23 13:42:47 +0200

    feat: privacy policy content & styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './features/home/home';
 import { LegalNotice } from './features/legal-notice/legal-notice';
+import { PrivacyPolicy } from './features/privacy-policy/privacy-policy';
 
 export const routes: Routes = [
   { path: '', component: Home },
-  { path: 'legal-notice', component: LegalNotice }
+  { path: 'legal-notice', component: LegalNotice },
+  { path: 'privacy-policy', component: PrivacyPolicy }
 ];
--- src/app/features/contact/contact.ts
+++ src/app/features/contact/contact.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink],
   templateUrl: './contact.html',
   styleUrls: ['./contact.scss']
 })
